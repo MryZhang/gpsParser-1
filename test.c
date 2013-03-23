a5ceb4bc1995e8b4a62ad5950c6b6530c5eb96a4
Admin <kywe665@gmail.com>
2013-03-23 13:25:21 -0600

    using strlen

--- test.c
+++ test.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h> 
 #include <math.h>
 
 void myFunction(char* in_string, char* out_string) {
   printf("Start_String=%s\n", in_string);
-  int in_size = (int)(sizeof(in_string));
+  int in_size = strlen(in_string); //TODO Left off here
   printf("%d\n", in_size);
   int i = 0;
   for(i = 0; i <= in_size-ceil(in_size/2); i++) {
