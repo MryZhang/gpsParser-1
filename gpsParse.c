1b8f8e689e4c3c4b414b875a13f69043d6d99e2c
Admin <kywe665@gmail.com>
2013-03-23 12:42:36 -0600

    Working

--- gpsParse.c
+++ gpsParse.c
@@ -22,6 +22,21 @@ double toDoubleTenths(char* s, int start, int stop) {
     }
     return atof(newChar);
 }
+double getSign(char sign_char) {
+    switch(sign_char){
+      case 'N':
+        return 1.0;
+      case 'S':
+        return -1.0;
+      case 'W':
+        return -1.0;
+      case 'E':
+        return 1.0;
+      default:
+        printf("%s\n", "BROKEN");
+      ;
+    }
+}
 
 int main(int arg) {
   typedef enum { false, true } bool;
@@ -31,7 +46,7 @@ int main(int arg) {
   double lon_value = 0.0;
   double temp_lon = 0.0;
   double temp_lat = 0.0;
-  printf("%d\n", (int)sizeof(in_string));
+//  printf("%d\n", (int)sizeof(in_string));
   int i;
   bool reading = false;
   int value_count = 0;
@@ -39,50 +54,53 @@ int main(int arg) {
   int value_seconds = 0;
   int value_end = 0;
   int minute_start = 0;
+  double sign = 0.0;
   for(i = 0; i <= sizeof(in_string); i++) {
     if(value_count < 2) {
-    printf("%c\n", in_string[i]);
+//    printf("%c\n", in_string[i]);
       switch(in_string[i]){
         case ',':
           if(!reading) {
             reading = true;
-            printf("%s\n","Start of value");
+//            printf("%s\n","Start of value");
             value_start = i+1;
           }
           else {
             reading = false;
-            printf("%s\n","End of value");
+//            printf("%s\n","End of value");
             value_end = i;
+            double sign = getSign(in_string[i+1]);
+//            printf("Sign=%f\n", sign);
             if(value_count == 0) {
               temp_lat += toDoubleTenths(in_string, value_seconds, i-1);
-              printf("temp_lat=%f",temp_lat);
+//              printf("temp_lat=%f\n",temp_lat);
               lat_value += temp_lat/60.0;
+              lat_value *= sign;
             }
             else {
               temp_lon += toDoubleTenths(in_string, value_seconds, i-1);
-              printf("temp_lon=%f",temp_lon);
+//              printf("temp_lon=%f\n",temp_lon);
               lon_value += temp_lon/60.0;
+              lon_value *= sign;
             }
             printf("LAT=%f LON=%f\n",lat_value, lon_value);
-            //get seconds
-            //value *= sign;//get sign
             value_count++;
           }
           break;
         case '.':
-          printf("%s\n","Found Decimal");
+//          printf("%s\n","Found Decimal");
           if(reading) {
             if(value_count == 0) {
               lat_value += toDouble(in_string, value_start, i-3);
               temp_lat += toDouble(in_string, i-2, i-1);
-              printf("temp_lat=%f",temp_lat);
+//              printf("temp_lat=%f\n",temp_lat);
             }
             else {
               lon_value += toDouble(in_string, value_start, i-3);
               temp_lon += toDouble(in_string, i-2, i-1);
-              printf("temp_lon=%f",temp_lon);
+//              printf("temp_lon=%f\n",temp_lon);
             }
-            printf("LAT=%f LON=%f\n",lat_value, lon_value);
+//            printf("LAT=%f LON=%f\n",lat_value, lon_value);
             value_seconds = i+1;
           }
           break;
@@ -91,6 +109,4 @@ int main(int arg) {
       }
     }
   }
-  printf("%s\n",in_string);
-  printf("%s\n",out_buffer);
 }
